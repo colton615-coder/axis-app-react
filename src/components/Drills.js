d621b335a2e8713cbfd30062fe791ca05388e6e4
colton615-coder <colton615@gmail.com>
2025-09-26 01:22:57 -0400

    Create Drills.js

---
+++ src/components/Drills.js
@@ -0,0 +1,12 @@
+// src/components/Drills.js
+
+function Drills() {
+  return (
+    <div>
+      <h2>Training Drills</h2>
+      <p>This is where the custom drills feature will be built.</p>
+    </div>
+  );
+}
+
+export default Drills;
