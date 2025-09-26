d1401f6f4299ffdf6e256d6d712bf3d6336eb6fe
colton615-coder <colton615@gmail.com>
2025-09-26 01:24:42 -0400

    Create Notepad.js

---
+++ src/components/Notepad.js
@@ -0,0 +1,12 @@
+// src/components/Notepad.js
+
+function Notepad() {
+  return (
+    <div>
+      <h2>Practice Notepad</h2>
+      <p>This is where the notepad feature will be built.</p>
+    </div>
+  );
+}
+
+export default Notepad;
